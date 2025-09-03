26677d53cbab25b43310bc56e98ef6a964ea0122
SvenFE <shensven2016@gmail.com>
2025-09-03 17:20:42 +0800

    chore(zustand): 移除 Redux DevTool 扩展

--- src/pages/AIConversation/components/useDeviceStore.js
+++ src/pages/AIConversation/components/useDeviceStore.js
@@ -1,14 +1,13 @@
-//设备状态管理
-
-
-import { create } from 'zustand'
-import { WavRecorder, WavStreamPlayer } from 'wavtools'
+// 设备状态管理
+import { create } from "zustand";
+import { WavRecorder, WavStreamPlayer } from "wavtools";
 
 const useDeviceStore = create(() => {
   return {
     wavRecorder: new WavRecorder({ sampleRate: 24000 }),
     wavStreamPlayer: new WavStreamPlayer({ sampleRate: 24000 }),
-  }
-})
+  };
+});
 
-export default useDeviceStore
+export default useDeviceStore;
+export { useDeviceStore };
