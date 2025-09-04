aa3a43225550b587fb525aebe2bec56c7acc5f9e
SvenFE <shensven2016@gmail.com>
2025-09-04 14:53:10 +0800

    feat: 增加一个 WebSocket 插槽，用于在 `/{user_id}/ai-conversation` 路由实例化 ws
    # Conflicts:
    #	src/pages/AIConversation/AIConversationPage.js

---
+++ src/pages/AIConversation/components/WebSocketSlot.jsx
@@ -0,0 +1,42 @@
+// WebSocket Slot
+
+import { useEffect } from "react";
+import useWebSocket from "react-use-websocket";
+import useConversationStore from "./useConversationStore.js";
+import useRealtimeMsgEffect from "./useRealtimeMsgEffect.js";
+
+function WebSocketSlot({ children }) {
+  const { wsConnected, setWsConnected, setWsInstance } = useConversationStore();
+
+  const apiKey = process.env.REACT_APP_STEP_API_KEY;
+  const baseUrl = process.env.REACT_APP_REALTIME_ENDPOINT;
+  const url = new URL(baseUrl ?? "wss://api.stepfun.com/v1/realtime");
+  url.searchParams.set("model", "step-1o-audio");
+  url.searchParams.set("apiKey", apiKey ?? "");
+
+  const wsInstance = useWebSocket(
+    url.href,
+    {
+      share: true,
+      heartbeat: {
+        message: "ping" + Date.now(),
+        interval: 15000,
+      },
+      onClose: () => {
+        setWsConnected(false);
+      },
+    },
+    wsConnected
+  );
+
+  useEffect(() => {
+    setWsInstance(wsInstance);
+  }, [wsInstance.readyState]);
+
+  useRealtimeMsgEffect(wsInstance);
+
+  return children;
+}
+
+export default WebSocketSlot;
+export { WebSocketSlot };
